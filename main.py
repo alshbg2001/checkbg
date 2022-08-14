fd754cff47006c3f01839518d7482ab8f56c0e5d
alshbg <109930669+alshbg2001@users.noreply.github.com>
2022-08-14 11:35:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,11 +29,9 @@ def start(bot, msg):
     if msg.chat.id == Dev:
         bot.send_message(
             msg.chat.id, f"welcom to bot\n check users telegram", reply_markup=reply_markup)
-        fo = open(str(msg.chat.id)+".txt", "r").read()
-        if len(str(fo)) <= 1:
-            fo = open(str(msg.chat.id)+".txt", "w")
-            fo.write("0")
-            fo.close()
+        fo = open(str(msg.chat.id)+".txt", "w")
+        fo.write("0")
+        fo.close()
     elif msg.chat.id != Dev:
         reply_markup = InlineKeyboardMarkup([
             [
