0f1938fea8214202c1e47085ca3b7c44ca3a2710
alshbg <109930669+alshbg2001@users.noreply.github.com>
2022-08-14 11:10:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 import requests
 import random
 from pyrogram import Client, filters
@@ -11,32 +12,44 @@ bot = Client(
     bot_token='5067621147:AAEeKEBjWuNMl82UJYOd_LgQE2WIcwTdttg'
 )
 
+Dev = 961743188
 
 @bot.on_message(filters.command('start'))
 def start(bot, msg):
     reply_markup = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton('start check users 3', callback_data=f"check#3"),
+            InlineKeyboardButton('start check users 3',
+                                 callback_data=f"check#3"),
         ],
         [
-            InlineKeyboardButton('start chech users 4', callback_data=f"check#4"),
+            InlineKeyboardButton('start chech users 4',
+                                 callback_data=f"check#4"),
         ],
     ])
-    bot.send_message(
-        msg.chat.id, f"welcom to bot\n check users telegram", reply_markup=reply_markup)
+    if msg.chat.id == Dev:
+        bot.send_message(
+            msg.chat.id, f"welcom to bot\n check users telegram", reply_markup=reply_markup)
+        fo = open(str(msg.chat.id)+".txt", "w")
+        fo.write("0")
+        fo.close()
 
 
 @bot.on_callback_query()
 def check_start(bot, CallbackQuery):
     global running
-    if CallbackQuery.data.split("#")[0] == "check":
+    fo = open(str(CallbackQuery.message.chat.id)+".txt", "r").read()
+    xx = 0
+    if CallbackQuery.data.split("#")[0] == "check" and len(str(fo)) <= 1 and CallbackQuery.message.chat.id == Dev:
+        fo = open(str(CallbackQuery.message.chat.id) +
+                  ".txt", "a").write(str(xx))
         running = True
         reply_markup = InlineKeyboardMarkup([
             [
                 InlineKeyboardButton('stop', callback_data=f"stop"),
             ],
             [
-                InlineKeyboardButton('start', callback_data=f"check#"+str(CallbackQuery.data.split("#")[1]))
+                InlineKeyboardButton(
+                    'start', callback_data=f"check#"+str(CallbackQuery.data.split("#")[1]))
             ],
         ])
         chat_id = CallbackQuery.message.chat.id
@@ -46,18 +59,23 @@ def check_start(bot, CallbackQuery):
         no = 0
         while running:
             ii += 1
-            user = str(''.join(random.choice(us) for i in range(int(CallbackQuery.data.split("#")[1]))))
+            user = str(''.join(random.choice(us)
+                       for i in range(int(CallbackQuery.data.split("#")[1]))))
             r = requests.get(
                 f"https://alsh-ax.ml/api/TelegramCheck.php?user={user}bot").json()
             if r['info']['status'] == "Available":
                 yes += 1
-                bot.send_message(chat_id, f"welcom.\n - is user Available : @{user}bot \n Dev @alsh_3k")
+                bot.send_message(
+                    chat_id, f"welcom.\n - is user Available : @{user}bot \n Dev @alsh_3k")
                 ok = "done"
             elif r['info']['status'] == "UnAvailable":
                 no += 1
                 ok = "field"
-            bot.edit_message_text(chat_id, CallbackQuery.message.id, f"Done Start. \n user : @{user}bot \n - checking : {ii} \n - done : {yes} \n - failed : {no}", reply_markup=reply_markup)
-    elif CallbackQuery.data == "stop":
+            bot.edit_message_text(chat_id, CallbackQuery.message.id,
+                                  f"Done Start. \n user : @{user}bot \n - checking : {ii} \n - done : {yes} \n - failed : {no}", reply_markup=reply_markup)
+    elif CallbackQuery.data == "stop" and CallbackQuery.message.chat.id == Dev:
+        os.remove(str(CallbackQuery.message.chat.id)+".txt")
+        fo = open(str(CallbackQuery.message.chat.id)+".txt", "w").write("0")
         running = False
         return running
 
